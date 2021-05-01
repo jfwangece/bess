74412f79fa356a23f539d881ad68fb80513da342
Jianfeng Wang <pkueewjf@gmail.com>
2021-04-30 23:03:54 -0700

    Update the system IPs

--- bessctl/conf/faas/reset_conf.sh
+++ bessctl/conf/faas/reset_conf.sh
@@ -8,5 +8,5 @@ sed -i "s/pcie_addr = ['81:00.0', '81:00.1']/pcie_addr = ['06:00.0', '06:00.1']/
 # faas_ingres
 
 sed -i "s/pcie0 = '06:00.0'/pcie0 = '06:00.0'/g" $(find . -type f -name "faas_ingress*.bess")
-sed -i "s/faas_ip = '128.105.144.219'/faas_ip = '128.105.145.255'/g" $(find . -type f -name "faas_ingress*.bess")
-sed -i "s/redis_ip = '128.105.145.193'/redis_ip = '128.105.145.196'/g" $(find . -type f -name "faas_ingress*.bess")
+sed -i "s/faas_ip = '128.105.145.255'/faas_ip = '128.105.145.252'/g" $(find . -type f -name "faas_ingress*.bess")
+sed -i "s/redis_ip = '128.105.145.196'/redis_ip = '128.105.145.181'/g" $(find . -type f -name "faas_ingress*.bess")
