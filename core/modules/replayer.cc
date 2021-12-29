9f4a5d32991eaedbc7b81118a49fceb5eb23f20f
Jianfeng Wang <pkueewjf@gmail.com>
2021-12-28 18:12:07 -0800

    Replayer: add a use_batching option to accelerate this module

--- core/modules/replayer.cc
+++ core/modules/replayer.cc
@@ -35,30 +35,74 @@ CommandResponse Replayer::Init(const bess::pb::ReplayerArg &arg) {
 
   use_trace_time_ = !(playback_rate_mpps_ > 0 || playback_rate_mbps_ > 0);
 
-  // Record the startup timestamp
-  startup_ts_ = tsc_to_us(rdtsc());
-  temp_pkt_cnt_ = 0;
-  last_rate_calc_ts_ = startup_ts_;
+  use_batching_ = true;
+  if (arg.use_batching() == false) {
+    use_batching_ = false;
+  }
 
+  temp_pkt_cnt_ = 0;
+  temp_bit_cnt_ = 0;
+  // Record the startup timestamp. Note: this module starts to send packets after 1 sec
   if (use_trace_time_) {
     curr_time_ = tsc_to_us(rdtsc());
+    startup_ts_ = tsc_to_us(rdtsc()) + 2000000;
   } else {
     curr_time_ = tsc_to_ns(rdtsc());
+    startup_ts_ = tsc_to_us(rdtsc()) + 2000000000;
   }
+  last_rate_calc_ts_ = startup_ts_;
   next_pkt_time_ = startup_ts_;
 
   return CommandSuccess();
 }
 
 void Replayer::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
-  uint64_t time_diff = 0;
-
   int cnt = batch->cnt();
-  for (int i = 0; i < cnt; i++) {
-    if (use_trace_time_) {
+
+  if (use_batching_) { // batching
+    WaitToSendPkt(batch->pkts()[cnt-1]);
+
+    if (playback_rate_mpps_ > 0) {
+      // Replay at a certain packet rate
+      next_pkt_time_ = curr_time_ + cnt * 1000 / playback_rate_mpps_;
+    } else if (playback_rate_mbps_ > 0) {
+      // Replay at a certain bit rate
+      uint64_t sum_bytes = 0;
+      for (int i = 0; i < cnt; i++) {
+        sum_bytes += batch->pkts()[i]->total_len();
+      }
+      next_pkt_time_ = curr_time_ + sum_bytes * 8000 / playback_rate_mbps_;
+    }
+
+    RunNextModule(ctx, batch);
+  } else { // non batching
+    for (int i = 0; i < cnt; i++) {
+      // A busy loop that sends a packet until the current time passes the packet's time.
+      WaitToSendPkt(batch->pkts()[i]);
+
+      EmitPacket(ctx, batch->pkts()[i], 0);
+      temp_pkt_cnt_ += 1;
+      temp_bit_cnt_ += batch->pkts()[i]->total_len() * 8;
+
+      if (playback_rate_mpps_ > 0) {
+        // Replay at a certain packet rate
+        next_pkt_time_ = curr_time_ + 1000 / playback_rate_mpps_;
+      } else if (playback_rate_mbps_ > 0) {
+        // Replay at a certain bit rate
+        next_pkt_time_ = curr_time_ + batch->pkts()[i]->total_len() * 8000 / playback_rate_mbps_;
+      }
+    }
+  }
+}
+
+// Return when the current time exceeds |pkt|'s departure time.
+// Note: if |use_trace_time_|, then it should first read the packet's departure time
+// from the packet's payload.
+void Replayer::WaitToSendPkt(bess::Packet *pkt) {
+  uint64_t time_diff = 0;
+  if (use_trace_time_) {
       // Read the timestamp in the TCP header.
-      time_diff = 0;
-      GetTimestamp(batch->pkts()[i], offset_, &time_diff);
+      GetTimestamp(pkt, offset_, &time_diff);
 
       if (time_diff) {
         if (time_diff > 60000000) {
@@ -80,21 +124,10 @@ checktime:
     }
 
     if (curr_time_ > next_pkt_time_) {
-      EmitPacket(ctx, batch->pkts()[i], 0);
-      temp_pkt_cnt_ += 1;
-      temp_bit_cnt_ += batch->pkts()[i]->total_len() * 8;
-
-      if (playback_rate_mpps_ > 0) {
-        // Replay at a certain packet rate
-        next_pkt_time_ = curr_time_ + 1000 / playback_rate_mpps_;
-      } else if (playback_rate_mbps_ > 0) {
-        // Replay at a certain bit rate
-        next_pkt_time_ = curr_time_ + batch->pkts()[i]->total_len() * 8000 / playback_rate_mbps_;
-      }
+      return;
     } else {
       goto checktime;
     }
-  }
 }
 
 ADD_MODULE(Replayer, "replayer",
