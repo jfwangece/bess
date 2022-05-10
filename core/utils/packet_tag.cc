e4491bfd42aa9760a9e14e398c58e2ed656119fa
Jianfeng Wang <pkueewjf@gmail.com>
2022-05-10 01:31:04 -0700

    utils/packet_tag: add a function to log a vector of packet tags to file
    This patch adds several helper functions to utils/packet_tag.

--- core/utils/packet_tag.cc
+++ core/utils/packet_tag.cc
@@ -1,10 +1,37 @@
 #include "packet_tag.h"
 
+#include <chrono>
+
 namespace bess{
 namespace utils{
-void TagPacketTimestamp(bess::Packet* pkt, size_t offset, uint64_t ts) {
-  uint64_t* ts_ptr;
-  const size_t kTagSize = sizeof(*ts_ptr);
+namespace {
+std::string CurrentDate() {
+  using sysclock_t = std::chrono::system_clock;
+  std::time_t now = sysclock_t::to_time_t(sysclock_t::now());
+  char buf[16] = {0};
+  std::strftime(buf, sizeof(buf), "%Y-%m-%d", std::localtime(&now));
+  return std::string(buf);
+}
+} // namespace
+
+void LogPacketTags(std::vector<PerPacketTag> tags) {
+  std::string now = CurrentDate();
+  std::string fname = "tags-" + now + ".txt";
+  std::ofstream out_fp(fname);
+  if (out_fp.is_open()) {
+    for (size_t i = 0; i < tags.size(); i++) {
+      out_fp << "packet:" << i;
+      out_fp << "idle:" << tags[i].rcore_idle_epoch_count;
+      out_fp << ", qlen:" << tags[i].sw_q_len;
+      out_fp << std::endl;
+    }
+  }
+  out_fp.close();
+}
+
+inline void TagUint64(bess::Packet* pkt, size_t offset, uint64_t val) {
+  uint64_t* ptr;
+  const size_t kTagSize = sizeof(*ptr);
   size_t room = pkt->data_len() - offset;
   if (room < kTagSize) {
     void *ret = pkt->append(kTagSize - room);
@@ -13,12 +40,37 @@ void TagPacketTimestamp(bess::Packet* pkt, size_t offset, uint64_t ts) {
       return;
     }
   }
-  ts_ptr = pkt->head_data<uint64_t *>(offset);
-  *ts_ptr = ts;
+  ptr = pkt->head_data<uint64_t*>(offset);
+  *ptr = val;
+}
+inline void GetUint64(bess::Packet* pkt, size_t offset, uint64_t *val) {
+  *val = *(pkt->head_data<uint64_t*>(offset));
+}
+
+inline void TagUint32(bess::Packet* pkt, size_t offset, uint32_t val) {
+  uint32_t* ptr;
+  const size_t kTagSize = sizeof(*ptr);
+  size_t room = pkt->data_len() - offset;
+  if (room < kTagSize) {
+    void *ret = pkt->append(kTagSize - room);
+    if (!ret) {
+      // not enough tailroom for timestamp. give up
+      return;
+    }
+  }
+  ptr = pkt->head_data<uint32_t*>(offset);
+  *ptr = val;
+}
+inline void GetUint32(bess::Packet* pkt, size_t offset, uint32_t *val) {
+  *val = *(pkt->head_data<uint32_t*>(offset));
+}
+
+void TagPacketTimestamp(bess::Packet* pkt, size_t offset, uint64_t ts) {
+  TagUint64(pkt, offset, ts);
 }
 
 void GetPacketTimestamp(bess::Packet* pkt, size_t offset, uint64_t *ts) {
-  *ts = *(pkt->head_data<uint64_t *>(offset));
+  GetUint64(pkt, offset, ts);
 }
 } // namespace utils
 } // namespace bess
--- core/utils/packet_tag.h
+++ core/utils/packet_tag.h
@@ -1,10 +1,23 @@
-#ifndef BESS_UTILS_SYS_MEASURE_H_
-#define BESS_UTILS_SYS_MEASURE_H_
+#ifndef BESS_UTILS_PACKET_TAG_H_
+#define BESS_UTILS_PACKET_TAG_H_
 
 #include "../packet.h"
 
 namespace bess{
 namespace utils{
+struct PerPacketTag {
+  PerPacketTag() = default;
+  int rcore_idle_epoch_count;
+  uint32_t sw_q_len;
+};
+
+void LogPacketTags(std::vector<PerPacketTag> tags);
+
+inline void TagUint64(bess::Packet* pkt, size_t offset, uint64_t val);
+inline void TagUint32(bess::Packet* pkt, size_t offset, uint32_t val);
+inline void GetUint64(bess::Packet* pkt, size_t offset, uint64_t *val);
+inline void GetUint32(bess::Packet* pkt, size_t offset, uint32_t *val);
+
 // Tag a timestamp to |pkt| at |offset| (in bytes)
 void TagPacketTimestamp(bess::Packet* pkt, size_t offset, uint64_t ts);
 
