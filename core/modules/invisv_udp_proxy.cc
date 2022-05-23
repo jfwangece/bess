18aabf6dd1c00679c03daf4795bb68fc109bd292
Jianfeng Wang <pkueewjf@gmail.com>
2022-05-23 12:16:21 -0700

    core/modules: implement INVISV UDP proxy on top of NAT
    INVISV's routing as a service (RaaS) works on top of HTTP3/QUIC.
    Therefore, it requires a multi-hop UDP proxy that sits in the cloud.
    
    This commit starts the implementation of INVISV UDP proxy as a BESS
    module. The UDP proxy is implemented on top of the default dynamic NAT
    module in BESS. This commit borrows the code from NAT, and creates a new
    BESS module, i.e. INVISVUDPProxy, as well as its own protobuf messages.

---
+++ core/modules/invisv_udp_proxy.cc
@@ -0,0 +1,428 @@
+// Copyright (c) 2016-2017, Nefeli Networks, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// * Neither the names of the copyright holders nor the names of their
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "invisv_udp_proxy.h"
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+
+#include "../utils/checksum.h"
+#include "../utils/common.h"
+#include "../utils/ether.h"
+#include "../utils/format.h"
+#include "../utils/icmp.h"
+#include "../utils/ip.h"
+#include "../utils/tcp.h"
+#include "../utils/udp.h"
+
+using bess::utils::Ethernet;
+using bess::utils::Ipv4;
+using IpProto = bess::utils::Ipv4::Proto;
+using bess::utils::Udp;
+using bess::utils::Tcp;
+using bess::utils::Icmp;
+using bess::utils::ChecksumIncrement16;
+using bess::utils::ChecksumIncrement32;
+using bess::utils::UpdateChecksumWithIncrement;
+using bess::utils::UpdateChecksum16;
+
+const Commands INVISVUDPProxy::cmds = {
+    {"get_initial_arg", "EmptyArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::GetInitialArg), Command::THREAD_SAFE},
+    {"get_runtime_config", "EmptyArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::GetRuntimeConfig), Command::THREAD_SAFE},
+    {"set_runtime_config", "EmptyArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::SetRuntimeConfig), Command::THREAD_SAFE},
+    {"set_proxy", "INVISVUDPProxySetProxyEndpointArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::SetUDPProxy), Command::THREAD_SAFE},
+    {"set_next_hop_proxy", "INVISVUDPProxySetProxyEndpointArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::SetNextHopUDPProxy),
+     Command::THREAD_SAFE},
+    {"get_proxy", "EmptyArg", MODULE_CMD_FUNC(&INVISVUDPProxy::GetUDPProxy),
+     Command::THREAD_SAFE},
+    {"get_next_hop_proxy", "EmptyArg",
+     MODULE_CMD_FUNC(&INVISVUDPProxy::GetNextHopUDPProxy),
+     Command::THREAD_SAFE},
+};
+
+namespace {
+static inline std::tuple<bool, Endpoint, Endpoint> ExtractEndpoint(
+                                              const Ipv4 *ip, const void *l4) {
+  IpProto proto = static_cast<IpProto>(ip->protocol);
+
+  if (likely(proto == IpProto::kUdp)) {
+    // UDP and TCP share the same layout for port numbers
+    const Udp *udp = static_cast<const Udp *>(l4);
+    Endpoint src = {.addr = ip->src, .port = udp->src_port, .protocol = proto};
+    Endpoint dst = {.addr = ip->dst, .port = udp->dst_port, .protocol = proto};
+
+    return std::make_tuple(true, src, dst);
+  }
+
+  return std::make_tuple(false,
+      Endpoint{.addr = ip->src, .port = be16_t(0), .protocol = 0},
+      Endpoint{.addr = ip->dst, .port = be16_t(0), .protocol = 0});
+}
+}
+
+// TODO(torek): move this to set/get runtime config
+CommandResponse INVISVUDPProxy::Init(const bess::pb::INVISVUDPProxyArg &arg) {
+  // Check before committing any changes.
+  for (const auto &address_range : arg.ext_addrs()) {
+    for (const auto &range : address_range.port_ranges()) {
+      if (range.begin() >= range.end() || range.begin() > UINT16_MAX ||
+          range.end() > UINT16_MAX) {
+        return CommandFailure(EINVAL, "Port range for address %s is malformed",
+                              address_range.ext_addr().c_str());
+      }
+    }
+  }
+
+  for (const auto &address_range : arg.ext_addrs()) {
+    auto ext_addr = address_range.ext_addr();
+    be32_t addr;
+
+    bool ret = bess::utils::ParseIpv4Address(ext_addr, &addr);
+    if (!ret) {
+      return CommandFailure(EINVAL, "invalid IP address %s", ext_addr.c_str());
+    }
+
+    ext_addrs_.push_back(addr);
+    // Add a port range list
+    std::vector<PortRange> port_list;
+    if (address_range.port_ranges().size() == 0) {
+      port_list.emplace_back(PortRange{
+          .begin = 0u, .end = 65535u, .suspended = false,
+      });
+    }
+    for (const auto &range : address_range.port_ranges()) {
+      port_list.emplace_back(PortRange{
+          .begin = (uint16_t)range.begin(),
+          .end = (uint16_t)range.end(),
+          // Control plane gets to decide if the port range can be used.
+          .suspended = range.suspended()});
+    }
+    port_ranges_.push_back(port_list);
+  }
+
+  if (ext_addrs_.empty()) {
+    return CommandFailure(EINVAL,
+                          "at least one external IP address must be specified");
+  }
+
+  // Sort so that GetInitialArg is predictable and consistent.
+  std::sort(ext_addrs_.begin(), ext_addrs_.end());
+
+  // Init |curr_udp_proxy_| and |next_hop_udp_proxy_|.
+  curr_udp_proxy_.addr = be32_t(0);
+  curr_udp_proxy_.port = be16_t(0);
+  curr_udp_proxy_.protocol = IpProto::kUdp;
+  next_hop_udp_proxy_.addr = be32_t(0);
+  next_hop_udp_proxy_.port = be16_t(0);
+  next_hop_udp_proxy_.protocol = IpProto::kUdp;
+
+  return CommandSuccess();
+}
+
+CommandResponse INVISVUDPProxy::GetInitialArg(const bess::pb::EmptyArg &) {
+  bess::pb::INVISVUDPProxyArg resp;
+  for (size_t i = 0; i < ext_addrs_.size(); i++) {
+    auto ext = resp.add_ext_addrs();
+    ext->set_ext_addr(ToIpv4Address(ext_addrs_[i]));
+    for (auto irange : port_ranges_[i]) {
+      auto erange = ext->add_port_ranges();
+      erange->set_begin((uint32_t)irange.begin);
+      erange->set_end((uint32_t)irange.end);
+      erange->set_suspended(irange.suspended);
+    }
+  }
+  return CommandSuccess(resp);
+}
+
+CommandResponse INVISVUDPProxy::GetRuntimeConfig(const bess::pb::EmptyArg &) {
+  return CommandSuccess();
+}
+
+CommandResponse INVISVUDPProxy::SetRuntimeConfig(const bess::pb::EmptyArg &) {
+  return CommandSuccess();
+}
+
+CommandResponse INVISVUDPProxy::SetUDPProxy(
+      const bess::pb::INVISVUDPProxySetProxyEndpointArg &arg) {
+  if (arg.proxy_addr().size() > 0) {
+    be32_t addr;
+    bool ret = bess::utils::ParseIpv4Address(arg.proxy_addr(), &addr);
+    if (!ret) {
+      return CommandFailure(EINVAL,
+          "invalid proxy IP address %s", arg.proxy_addr().c_str());
+    }
+    curr_udp_proxy_.addr = addr;
+    curr_udp_proxy_.port = be16_t(arg.proxy_port());
+    curr_udp_proxy_.protocol = IpProto::kUdp;
+    return CommandSuccess();
+  }
+  return CommandFailure(EINVAL, "Incorrect proxy endpoint");
+}
+
+CommandResponse INVISVUDPProxy::SetNextHopUDPProxy(
+      const bess::pb::INVISVUDPProxySetProxyEndpointArg &arg) {
+  if (arg.proxy_addr().size() > 0) {
+    be32_t addr;
+    bool ret = bess::utils::ParseIpv4Address(arg.proxy_addr(), &addr);
+    if (!ret) {
+      return CommandFailure(EINVAL,
+          "invalid next-hop proxy IP address %s", arg.proxy_addr().c_str());
+    }
+    next_hop_udp_proxy_.addr = addr;
+    next_hop_udp_proxy_.port = be16_t(arg.proxy_port());
+    next_hop_udp_proxy_.protocol = IpProto::kUdp;
+    return CommandSuccess();
+  }
+  return CommandFailure(EINVAL, "Incorrect next-hop proxy endpoint");
+}
+
+CommandResponse INVISVUDPProxy::GetUDPProxy(const bess::pb::EmptyArg &) {
+  bess::pb::INVISVUDPProxyGetProxyEndpointArg r;
+  r.set_proxy_addr(ToIpv4Address(curr_udp_proxy_.addr));
+  r.set_proxy_port(curr_udp_proxy_.port.value());
+  return CommandSuccess(r);
+}
+
+CommandResponse INVISVUDPProxy::GetNextHopUDPProxy(const bess::pb::EmptyArg &) {
+  bess::pb::INVISVUDPProxyGetProxyEndpointArg r;
+  r.set_proxy_addr(ToIpv4Address(next_hop_udp_proxy_.addr));
+  r.set_proxy_port(next_hop_udp_proxy_.port.value());
+  return CommandSuccess(r);
+}
+
+// Not necessary to inline this function, since it is less frequently called
+INVISVUDPProxy::HashTable::Entry *INVISVUDPProxy::CreateNewEntry(
+                                           const Endpoint &src_internal,
+                                           uint64_t now) {
+  Endpoint src_external;
+
+  // This UDP proxy sends packets as if they were sent by the client.
+  // Each client endpoint is mapped to the current UDP proxy's IP and
+  // an un-used UDP port.
+  src_external.addr = curr_udp_proxy_.addr;
+  src_external.protocol = src_internal.protocol;
+
+  // |port_ranges_| must have at least one element.
+  for (const auto &port_range : port_ranges_[0]) {
+    uint16_t min;
+    uint16_t range;  // consider [min, min + range) port range
+    // Avoid allocation from an unusable range. We do this even when a range is
+    // already in use since we might want to reclaim it once flows die out.
+    if (port_range.suspended) {
+      continue;
+    }
+
+    if (src_internal.port == be16_t(0)) {
+      // ignore port number 0
+      return nullptr;
+    } else if (src_internal.port & ~be16_t(1023)) {
+      if (port_range.end <= 1024u) {
+        continue;
+      }
+      min = std::max((uint16_t)1024, port_range.begin);
+      range = port_range.end - min + 1;
+    } else {
+      // Privileged ports are mapped to privileged ports (rfc4787 REQ-5-a)
+      if (port_range.begin >= 1023u) {
+        continue;
+      }
+      min = port_range.begin;
+      range = std::min((uint16_t)1023, port_range.end) - min;
+    }
+
+    // Start from a random port, then do linear probing
+    uint16_t start_port = min + rng_.GetRange(range);
+    uint16_t port = start_port;
+    int trials = 0;
+
+    std::lock_guard<std::mutex> guard(map_lock_);
+    do {
+      src_external.port = be16_t(port);
+      auto *hash_reverse = map_.Find(src_external);
+      if (src_external.port != curr_udp_proxy_.port &&
+          hash_reverse == nullptr) {
+      found:
+        // Found an available src_internal <-> src_external mapping
+        NatEntry forward_entry;
+        NatEntry reverse_entry;
+
+        reverse_entry.endpoint = src_internal;
+        map_.Insert(src_external, reverse_entry);
+
+        forward_entry.endpoint = src_external;
+        return map_.Insert(src_internal, forward_entry);
+      } else {
+        // A':a' is not free, but it might have been expired.
+        // Check with the forward hash entry since timestamp refreshes only for
+        // forward direction.
+        auto *hash_forward = map_.Find(hash_reverse->second.endpoint);
+
+        // Forward and reverse entries must share the same lifespan.
+        DCHECK(hash_forward != nullptr);
+
+        if (now - hash_forward->second.last_refresh > kTimeOutNs) {
+          // Found an expired mapping. Remove A':a' <-> A'':a''...
+          map_.Remove(hash_forward->first);
+          map_.Remove(hash_reverse->first);
+          goto found;  // and go install A:a <-> A':a'
+        }
+      }
+
+      port++;
+      trials++;
+
+      // Out of range? Also check if zero due to uint16_t overflow
+      if (port == 0 || port >= min + range) {
+        port = min;
+      }
+      // FIXME: Should not try for kMaxTrials.
+    } while (port != start_port && trials < kMaxTrials);
+  }
+  return nullptr;
+}
+
+inline void Stamp(Ipv4 *ip, void *l4,
+                  const Endpoint &old_src, const Endpoint &old_dst,
+                  const Endpoint &new_src, const Endpoint &new_dst) {
+  IpProto proto = static_cast<IpProto>(ip->protocol);
+  DCHECK_EQ(old_src.protocol, new_src.protocol);
+  DCHECK_EQ(old_src.protocol, proto);
+
+  ip->src = new_src.addr;
+  ip->dst = new_dst.addr;
+
+  uint32_t l3_increment =
+    ChecksumIncrement32(old_src.addr.raw_value(), new_src.addr.raw_value()) +
+    ChecksumIncrement32(old_dst.addr.raw_value(), new_dst.addr.raw_value());
+  ip->checksum = UpdateChecksumWithIncrement(ip->checksum, l3_increment);
+
+  uint32_t l4_increment = l3_increment +
+    ChecksumIncrement16(old_src.port.raw_value(), new_src.port.raw_value()) +
+    ChecksumIncrement16(old_dst.port.raw_value(), new_dst.port.raw_value());
+
+  Udp *udp = static_cast<Udp *>(l4);
+  udp->src_port = new_src.port;
+  udp->dst_port = new_dst.port;
+
+  if (proto == IpProto::kTcp) {
+    Tcp *tcp = static_cast<Tcp *>(l4);
+    tcp->checksum = UpdateChecksumWithIncrement(tcp->checksum, l4_increment);
+  } else {
+    // NOTE: UDP checksum is tricky in two ways:
+    // 1. if the old checksum field was 0 (not set), no need to update
+    // 2. if the updated value is 0, use 0xffff (rfc768)
+    if (udp->checksum != 0) {
+      udp->checksum =
+          UpdateChecksumWithIncrement(udp->checksum, l4_increment) ?: 0xffff;
+    }
+  }
+}
+
+void INVISVUDPProxy::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
+  gate_idx_t igate_idx = ctx->current_igate;
+  gate_idx_t ogate_idx = GetOGate(igate_idx);
+
+  uint64_t now = ctx->current_ns;
+
+  int cnt = batch->cnt();
+  for (int i = 0; i < cnt; i++) {
+    bess::Packet *pkt = batch->pkts()[i];
+
+    Ethernet *eth = pkt->head_data<Ethernet *>();
+    Ipv4 *ip = reinterpret_cast<Ipv4 *>(eth + 1);
+    size_t ip_bytes = (ip->header_length) << 2;
+    void *l4 = reinterpret_cast<uint8_t *>(ip) + ip_bytes;
+
+    bool valid_protocol;
+    Endpoint src, dst;
+    std::tie(valid_protocol, src, dst) = ExtractEndpoint(ip, l4);
+
+    if (!valid_protocol) {
+      DropPacket(ctx, pkt);
+      continue;
+    }
+
+    Direction dir = kReverse;
+    if (!IsReverseTraffic(src, dst)) {
+      if (!IsForwardTraffic()) {
+        DropPacket(ctx, pkt);
+        continue;
+      }
+      dir = kForward;
+    }
+
+    Endpoint hash_key = dir == kForward ? src : dst;
+    map_lock_.lock();
+    auto *hash_item = map_.Find(hash_key);
+    map_lock_.unlock();
+
+    if (hash_item == nullptr) {
+      if (dir != kForward || !(hash_item = CreateNewEntry(src, now))) {
+        DropPacket(ctx, pkt);
+        continue;
+      }
+    }
+
+    // only refresh for outbound packets, rfc4787 REQ-6
+    if (dir == kForward) {
+      hash_item->second.last_refresh = now;
+      Stamp(ip, l4, src, dst, hash_item->second.endpoint, next_hop_udp_proxy_);
+    } else {
+      Stamp(ip, l4, src, dst, curr_udp_proxy_, hash_item->second.endpoint);
+    }
+
+    EmitPacket(ctx, pkt, ogate_idx);
+  }
+}
+
+bool INVISVUDPProxy::IsForwardTraffic() {
+  return true;
+}
+
+bool INVISVUDPProxy::IsReverseTraffic(Endpoint &src, Endpoint &dst) {
+  Endpoint::EqualTo eq;
+  if (eq(src, next_hop_udp_proxy_) && dst.addr == curr_udp_proxy_.addr) {
+    return true;
+  }
+  return false;
+}
+
+std::string INVISVUDPProxy::GetDesc() const {
+  // Divide by 2 since the table has both forward and reverse entries
+  return bess::utils::Format("%zu entries", map_.Count() / 2);
+}
+
+ADD_MODULE(INVISVUDPProxy, "udp_proxy",
+           "Dynamic network address/port translator for UDP traffic")
---
+++ core/modules/invisv_udp_proxy.h
@@ -0,0 +1,210 @@
+// Copyright (c) 2016-2017, Nefeli Networks, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// * Neither the names of the copyright holders nor the names of their
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef BESS_MODULES_INVISV_UDP_PROXY_H_
+#define BESS_MODULES_INVISV_UDP_PROXY_H_
+
+#include "../module.h"
+#include "../pb/module_msg.pb.h"
+
+#include <rte_config.h>
+#include <rte_hash_crc.h>
+
+#include <map>
+#include <mutex>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "../utils/cuckoo_map.h"
+#include "../utils/endian.h"
+#include "../utils/random.h"
+
+// Theory of operation:
+//
+// Definitions:
+// Endpoint = <IPv4 address, port>
+// UDP proxy = <proxy ingress IPv4 address, proxy ingress UDP port>
+// next-hop UDP proxy = <n-proxy IPv4 address, n-proxy UDP port>
+// Forward direction:
+// [Endpoint -> UDP proxy] ==> [UDP proxy -> next-hop UDP proxy]
+// Reverse direction:
+// [next-hop UDP proxy -> UDP proxy] ==> [UDP proxy -> Endpoint]
+//
+// There is a single hash table of Endpoint -> (Endpoint, timestamp), which
+// contains both forward and reverse mapping. They have the same lifespan.
+// (e.g., if one entry is deleted, its peer is also deleted)
+//
+// Note: The UDP proxy always has a next-hop UDP proxy C:c.
+// Suppose the table is empty, and we see a packet A:a ===> B:b.
+// The UDP proxy represents the endpoint A:a to communicate with C:c.
+// To do so, we find a free UDP port b' from the pool and create two entries:
+// - entry 1  A:a -> B:b'
+// - entry 2  B:b' -> A:a
+// Then the packet is updated to [A:a, B:b] ===> [B:b', C:c] (with entry 1).
+// When a return packet [C:c, B:b'] comes in, the destination (since it is
+// reverse dir) endpoint is [B:b, A:a] (with entry 2).
+
+using bess::utils::be16_t;
+using bess::utils::be32_t;
+
+struct alignas(8) Endpoint {
+  be32_t addr;
+
+  // TCP/UDP port number or ICMP identifier
+  be16_t port;
+
+  // L4 protocol (IPPROTO_*). Note that this is a 1-byte field in the IP header,
+  // but we store the value in a 2-byte field so that the struct be 8-byte long
+  // without a hole, without needing to initialize it explicitly.
+  uint16_t protocol;
+
+  struct Hash {
+    std::size_t operator()(const Endpoint &e) const {
+#if __x86_64
+      return crc32c_sse42_u64(
+          (static_cast<uint64_t>(e.addr.raw_value()) << 32) |
+              (static_cast<uint64_t>(e.port.raw_value()) << 16) |
+              static_cast<uint64_t>(e.protocol),
+          0);
+#else
+      return rte_hash_crc(&e, sizeof(uint64_t), 0);
+#endif
+    }
+  };
+
+  struct EqualTo {
+    bool operator()(const Endpoint &lhs, const Endpoint &rhs) const {
+      const union {
+        Endpoint endpoint;
+        uint64_t u64;
+      } &left = {.endpoint = lhs}, &right = {.endpoint = rhs};
+
+      return left.u64 == right.u64;
+    }
+  };
+};
+
+static_assert(sizeof(Endpoint) == sizeof(uint64_t), "Incorrect Endpoint");
+
+struct NatEntry {
+  Endpoint endpoint;
+
+  // last_refresh is only updated for forward-direction (outbound) packets, as
+  // per rfc4787 REQ-6. Reverse entries will have an garbage value.
+  // We do lazy reclaim of expired; NAT mapping entry will NOT expire unless it
+  // runs out of ports in the pool.
+  uint64_t last_refresh;  // in nanoseconds (ctx.current_ns)
+};
+
+// Port ranges are used to scale out the NAT.
+struct PortRange {
+  // Start of port range.
+  uint16_t begin;
+  // End of port range (exclusive).
+  uint16_t end;
+  // Is range usable, i.e., can we safely give out ports.
+  bool suspended;
+};
+
+// NAT module. 2 igates and 2 ogates
+// igate/ogate 0: forward dir
+// igate/ogate 1: reverse dir
+class INVISVUDPProxy final : public Module {
+ public:
+  enum Direction {
+    kForward = 0,  // internal -> external
+    kReverse = 1,  // external -> internal
+  };
+
+  static const gate_idx_t kNumOGates = Worker::kMaxWorkers;
+  static const gate_idx_t kNumIGates = Worker::kMaxWorkers;
+
+  static const Commands cmds;
+
+  INVISVUDPProxy()
+      : Module() {
+    Endpoint curr_udp_proxy_;
+    Endpoint next_hop_udp_proxy_;
+    max_allowed_workers_ = Worker::kMaxWorkers;
+  }
+
+  CommandResponse Init(const bess::pb::INVISVUDPProxyArg &arg);
+  CommandResponse GetInitialArg(const bess::pb::EmptyArg &arg);
+  CommandResponse GetRuntimeConfig(const bess::pb::EmptyArg &arg);
+  CommandResponse SetRuntimeConfig(const bess::pb::EmptyArg &arg);
+  CommandResponse SetUDPProxy(
+              const bess::pb::INVISVUDPProxySetProxyEndpointArg &arg);
+  CommandResponse SetNextHopUDPProxy(
+              const bess::pb::INVISVUDPProxySetProxyEndpointArg &arg);
+  CommandResponse GetUDPProxy(const bess::pb::EmptyArg &);
+  CommandResponse GetNextHopUDPProxy(const bess::pb::EmptyArg &);
+
+  void ProcessBatch(Context *ctx, bess::PacketBatch *batch) override;
+
+  // returns the number of active NAT entries (flows)
+  std::string GetDesc() const override;
+
+ private:
+  using HashTable = bess::utils::CuckooMap<Endpoint, NatEntry, Endpoint::Hash,
+                                           Endpoint::EqualTo>;
+
+  // 5 minutes for entry expiration (rfc4787 REQ-5-c)
+  static const uint64_t kTimeOutNs = 300ull * 1000 * 1000 * 1000;
+
+  // how many times shall we try to find a free port number?
+  static const int kMaxTrials = 128;
+
+  HashTable::Entry *CreateNewEntry(const Endpoint &internal, uint64_t now);
+
+  bool IsForwardTraffic();
+
+  // If |src| equals to |next_hop_udp_proxy_| and
+  // |dst|'s IP matches |curr_udp_proxy_| IP, return true. Otherwise, false.
+  bool IsReverseTraffic(Endpoint &src, Endpoint &dst);
+
+  // |igate| is coupled with the ogate at the same index
+  inline gate_idx_t GetOGate(gate_idx_t igate) { return igate; }
+
+  std::vector<be32_t> ext_addrs_;
+
+  Endpoint curr_udp_proxy_;
+  Endpoint next_hop_udp_proxy_;
+
+  // Port ranges available for each address. The first index is the same as the
+  // ext_addrs_ range.
+  std::vector<std::vector<PortRange>> port_ranges_;
+
+  std::mutex map_lock_;
+  HashTable map_;
+  Random rng_;
+};
+
+#endif  // BESS_MODULES_INVISV_UDP_PROXY_H_
