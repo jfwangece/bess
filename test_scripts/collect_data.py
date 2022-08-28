232db2943bc8b9ae57b8625c8121655683d464c0
Jianfeng Wang <pkueewjf@gmail.com>
2022-08-27 21:33:26 -0700

    nfvctl: prepare for the long-term NF profiling

--- test_scripts/collect_data.py
+++ test_scripts/collect_data.py
@@ -1,34 +1,34 @@
-'''
-python collect_data.py
+# This script is used to run tests and collect data.
+# It has been designed to run the following pipeline:
+# fg-> timestamp -> port_out
+# port_in -> nfv_monitor -> bypass -> measure -> sink
+# The port out is looped back to port in. The entire pipeline
+# is present in hw_ts_test.bess file.
+# It collects stats from nfv_monitor and the measure module.
+# This data gets written to a file named "data"
+# This script can be used as a template to write more automations
+# and collect test data
+# The script uses bessctl to interact with bessd and collect data
+# by writing the output to files.
+#
+# To run the script, do the following:
+# python collect_data.py
 
-This script is used to run tests and collect data.
-It has been designed to run the following pipeline:
-fg-> timestamp -> port_out
-port_in -> nfv_monitor -> bypass -> measure -> sink
-
-The port out is looped back to port in. The entire pipeline
-is present in hw_ts_test.bess file.
-
-It collects stats from nfv_monitor and the measure module.
-This data gets written to a file named "data"
-
-This script can be used as a template to write more automations
-and collect test data
-
-The script uses bessctl to interact with bessd and collect data
-by writing the output to files.
-'''
 import os
 from time import sleep
+from datetime import datetime
 import subprocess
 import re
+
+BESS_DIR = subprocess.check_output(["git", "rev-parse", "--show-toplevel"])[:-1]
+BESS_DIR = os.path.abspath(BESS_DIR) + "/"
+BESSCTL_DIR = BESS_DIR + "bin/bessctl"
 
 run_time = 10
 reset_time = 5
 pps_sample = [10000,50000,100000]
 pcycle_sample = [200,2000, 20000, 200000]
 num_runs = 3
-
 
 def get_hw_latency(text):
     latency = []
@@ -45,24 +45,24 @@ def get_pipline_latency(text):
     latency = [float(x.split(':')[-1]) for x in pat]
     return latency
 
-
 def do_test(i,pps, pcycle):
-    os.system("../bin/bessctl daemon stop")
-    os.system("../bin/bessctl daemon start")
-    with open("data", 'a') as f:
-        f.write("Run %d --- params: pps=%d pcycle=%d\n"%(i, pps,pcycle))
-    os.system("../bin/bessctl 'run samples/hw_ts_test BESS_PKT_SIZE=200,BESS_PKT_CYCLE="+str(pcycle)+",BESS_PKT_RATE="+str(pps)+"'")
+    output_fname = datetime.now().strftime('%Y-%m-%d-%H%M%S') + ".dat"
+    os.system(BESSCTL_DIR + " daemon stop")
+    os.system(BESSCTL_DIR + " daemon start")
+    with open(output_fname, 'a') as f:
+        f.write("Run %d --- params: pps=%d pcycle=%d\n"%(i, pps, pcycle))
+    os.system(BESSCTL_DIR + " 'run samples/hw_ts_test BESS_PKT_SIZE=200,BESS_PKT_CYCLE="+str(pcycle)+",BESS_PKT_RATE="+str(pps)+"'")
     sleep(reset_time)
-    os.system("../bin/bessctl 'command module nfv_monitor clear EmptyArg'")
-    os.system("../bin/bessctl 'command module measure0 clear EmptyArg'")
+    os.system(BESSCTL_DIR + " 'command module nfv_monitor clear EmptyArg'")
+    os.system(BESSCTL_DIR + " 'command module measure0 clear EmptyArg'")
     sleep(run_time)
-    os.system("../bin/bessctl 'command module nfv_monitor get_summary EmptyArg'")
-    os.system("../bin/bessctl 'command module measure0 get_summary MeasureCommandGetSummaryArg {\"latency_percentiles\":[50,99]}' > output")
-    (out,err) = subprocess.Popen(["cat", "stats.txt"], stdout=subprocess.PIPE).communicate()
+    os.system(BESSCTL_DIR + " 'command module nfv_monitor get_summary EmptyArg'")
+    os.system(BESSCTL_DIR + " 'command module measure0 get_summary MeasureCommandGetSummaryArg {\"latency_percentiles\":[50,99]}' > output")
+    (out, err) = subprocess.Popen(["cat", "stats.txt"], stdout=subprocess.PIPE).communicate()
     hw_latency = get_hw_latency(out)
-    (out,err) = subprocess.Popen(["cat", "output"], stdout=subprocess.PIPE).communicate()
+    (out, err) = subprocess.Popen(["cat", "output"], stdout=subprocess.PIPE).communicate()
     latency = get_pipline_latency(out)
-    with open("data",'a') as f:
+    with open(output_fname,'a') as f:
         f.write("%d,%d,%d,%d\n"%(hw_latency[0], latency[0], hw_latency[1], latency[1]))
     print(hw_latency, latency)
 
@@ -73,9 +73,10 @@ def run_test():
         for pcycle in pcycle_sample:
             for i in range(num_runs):
                 do_test(i,pps,pcycle)
-            
 
 def clear_system():
     os.system("rm -f stats.txt data output")
-clear_system()
-run_test()
+
+if __name__ == "__main__":
+    clear_system()
+    run_test()
