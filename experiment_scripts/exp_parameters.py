a6c1124ca4fb9db676ea07f286da2664d33abbe2
Jianfeng Wang <pkueewjf@gmail.com>
2022-09-01 23:47:57 -0700

    profile: add the long-term NF profiling script

--- experiment_scripts/exp_parameters.py
+++ experiment_scripts/exp_parameters.py
@@ -14,3 +14,9 @@ flow_count_samples = [1000, 2000]
 pkt_size_samples = range(150, 1500, 150)
 pkt_rate_samples = [1600000]
 flow_count_samples = [1000, 2000]
+
+## Long-term NF profiling
+# Chain 4
+pkt_size_samples = range(150, 1500, 300)
+pkt_rate_samples = range(1600000, 2400000, 50000)
+flow_count_samples = range(900, 3400, 300)
--- experiment_scripts/nf_profiler.py
+++ experiment_scripts/nf_profiler.py
@@ -3,13 +3,13 @@ import sys
 from bess_runner import run_test
 from profile_plot import plot_nf_profile
 
-pkt_size_samples = range(150, 1500, 150)
-pkt_rate_samples = [140000]
-flow_count_samples = [1000, 2000]
+# pkt_size_samples = range(150, 1500, 150)
+# pkt_rate_samples = [140000]
+# flow_count_samples = [1000, 2000]
 
-# pkt_size_samples = range(150, 1500, 300)
-# pkt_rate_samples = range(1500000, 2500000, 50000)
-# flow_count_samples = range(600, 3000, 200)
+pkt_size_samples = range(150, 1500, 300)
+pkt_rate_samples = range(1600000, 2400000, 50000)
+flow_count_samples = range(900, 3400, 300)
 
 def main():
     run_test("nfvctrl/profile_chain4", pkt_size_samples, pkt_rate_samples, flow_count_samples)
@@ -25,3 +25,9 @@ if __name__ == "__main__":
         elif sys.argv[1] == "exp" and len(sys.argv) == 3:
             exp_name = "nfvctrl/%s" %(sys.argv[2])
             run_test(exp_name, pkt_size_samples, pkt_rate_samples, flow_count_samples)
+        elif sys.argv[1] == "est":
+            num_groups = len(pkt_size_samples) * len(pkt_rate_samples) * len(flow_count_samples)
+            est_time = num_groups * 30.0 / 3600
+            print("Total exp groups: {}; Est time: {} hour(s)".format(num_groups, est_time))
+        else:
+            print("Unknown request: {}", sys.argv[1])
