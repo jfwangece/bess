8fa44f311f4ce29bc08a7971726e6f2d616fe322
Jianfeng Wang <pkueewjf@gmail.com>
2022-10-13 17:12:13 -0700

    exp

--- bessctl/cloudlab-exp.py
+++ bessctl/cloudlab-exp.py
@@ -14,16 +14,16 @@ worker_ip = ["128.110.219.135"]
 all_ip = traffic_ip + worker_ip
 
 def run_remote_command(ip, cmd):
-    remote_cmd = ['ssh', '-tt', 'uscnsl@{}'.format(ip), '"{}"'.format(cmd), '>/dev/null 2>&1', '\n']
+    remote_cmd = ['ssh', 'uscnsl@{}'.format(ip), '"{}"'.format(cmd), '>/dev/null', '2>&1', '\n']
     os.system(' '.join(remote_cmd))
     return
 
 def run_remote_besscmd(ip, cmds):
-    cmds.append('\n')
     cmds_str = u' '.join(cmds)
     remote_cmds = ['./bessctl/bessctl.py', ip, cmds_str, '\n']
-    os.system(' '.join(remote_cmds))
-    # run_cli(ip, io.StringIO('\n'.join(list_cmds)))
+    p = subprocess.Popen(remote_cmds, universal_newlines=True,
+                        stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return p
 
 def start_remote_bessd(ip):
     bessd = "/local/bess/core/bessd"
@@ -33,8 +33,8 @@ def start_remote_bessd(ip):
 
 def setup_remote_hugepage(ip):
     cmd1 = "echo 4096 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
-    cmd2 = "echo 4096 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages"
     run_remote_command(ip, cmd1)
+    cmd2 = "echo 4096 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages"
     run_remote_command(ip, cmd2)
     return
 
@@ -44,11 +44,15 @@ def start_traffic(tip):
 
 def start_ironside_worker(wip):
     cmds = ["run", "nfvctrl/cloud_chain4"]
-    run_remote_besscmd(wip, cmds)
+    p = run_remote_besscmd(wip, cmds)
+    out, err = p.communicate()
+    print("ironside {} starts".format(wip))
 
 def parse_latency_result(tip):
     cmds = ['command', 'module', 'measure0', 'get_summary', 'MeasureCommandGetSummaryArg', '{"latency_percentiles": [50.0, 90.0, 95.0, 98, 99.0]}']
-    run_remote_besscmd(tip, cmds)
+    p = run_remote_besscmd(tip, cmds)
+    out, err = p.communicate()
+    print(out + "\n")
 
 def main():
     # Prepare all servers
@@ -62,7 +66,6 @@ def main():
         p = multiprocessing.Process(target=start_remote_bessd, args=(tip,))
         p.start()
         pids.append(p)
-
     for wip in worker_ip:
         p = multiprocessing.Process(target=start_remote_bessd, args=(wip,))
         p.start()
@@ -73,15 +76,21 @@ def main():
     print("exp: all bessd started\n")
 
     # Run all workers
+    pids = []
     for wip in worker_ip:
-        start_ironside_worker(wip)
-    print("exp: all workers started")
-    return
+        p = multiprocessing.Process(target=start_ironside_worker, args=(wip,))
+        p.start()
+        pids.append(p)
+
+    for p in pids:
+        p.join()
+    print("exp: all workers started\n")
+
     for tip in traffic_ip:
         start_traffic(tip)
-    print("exp: traffic started")
+    print("exp: traffic started\n")
 
-    time.sleep(2)
+    time.sleep(30)
 
     parse_latency_result(traffic_ip[0])
     return
