e71fbc9f57cec2ce051d01439aeb048658ffdfc2
Jianfeng Wang <pkueewjf@gmail.com>
2022-10-14 01:26:31 -0700

    cloudlab: exp with hyperthreading off

--- bessctl/cloudlab-exp.py
+++ bessctl/cloudlab-exp.py
@@ -31,11 +31,14 @@ def run_remote_besscmd(ip, cmds):
                         stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p
 
-def start_remote_bessd(ip):
-    bessd = "/local/bess/core/bessd"
-    bessd_cmd = "sudo {} --dpdk=true --buffers=262144 -k".format(bessd)
-    run_remote_command(ip, bessd_cmd)
-    return
+
+def reset_grub(ip):
+    print("Reset grub conf for {}".format(ip))
+
+    cmd = "git clone https://github.com/jwangee/FaaS-Setup.git"
+    run_remote_command(ip, cmd)
+    cmd = "cd ./FaaS-Setup && git pull && ./ironside-update-grub.sh"
+    run_remote_command(ip, cmd)
 
 def install_mlnx(ip):
     print("Install MLNX OFED for {}".format(ip))
@@ -59,25 +62,38 @@ def install_bess(ip):
     run_remote_command(ip, cmd)
     return
 
-def setup_remote_hugepage(ip):
-    print("Setup hugepages for {}".format(ip))
+def setup_cpu_memory(ip):
+    print("Setup hyperthread for {}".format(ip))
+    cmd = "git clone https://github.com/jwangee/FaaS-Setup.git"
+    run_remote_command(ip, cmd)
+    cmd = "cd ./FaaS-Setup && git pull && ./ironside-hyperthread.sh"
+    run_remote_command(ip, cmd)
 
-    cmd1 = "echo 4096 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
+    print("Setup hugepages for {}".format(ip))
+    cmd1 = "echo 8 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages"
     run_remote_command(ip, cmd1)
-    cmd2 = "echo 4096 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages"
+    cmd2 = "echo 8 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages"
     run_remote_command(ip, cmd2)
+    return
+
+def start_remote_bessd(ip):
+    bessd = "/local/bess/core/bessd"
+    bessd_cmd = "sudo {} --dpdk=true --buffers=262144 -k".format(bessd)
+    run_remote_command(ip, bessd_cmd)
     return
 
 def start_traffic(tip):
     cmds = ["run", "nfvctrl/cloud_pcap_replay"]
     p = run_remote_besscmd(tip, cmds)
     out, err = p.communicate()
+    print(out)
     print("traffic {} starts".format(tip))
 
 def start_ironside_worker(wip):
     cmds = ["run", "nfvctrl/cloud_chain4"]
     p = run_remote_besscmd(wip, cmds)
     out, err = p.communicate()
+    print(out)
     print("ironside worker {} starts".format(wip))
 
 def parse_latency_result(tip):
@@ -85,6 +101,20 @@ def parse_latency_result(tip):
     p = run_remote_besscmd(tip, cmds)
     out, err = p.communicate()
     print(out + "\n")
+
+def reset_grub_for_all():
+    # install mlnx ofed
+    pids = []
+    for ip in all_ip:
+        p = multiprocessing.Process(target=reset_grub, args=(ip,))
+        p.start()
+        pids.append(p)
+
+    for p in pids:
+        p.join()
+
+    print("Done resetting grub")
+    return
 
 def install_mlnx_for_all():
     # install mlnx ofed
@@ -114,11 +144,11 @@ def install_bess_for_all():
     print("Done installing ironside")
     return
 
-def setup_hugepage_for_all():
+def setup_cpu_hugepage_for_all():
     # hugepage all servers
     pids = []
     for ip in all_ip:
-        p = multiprocessing.Process(target=setup_remote_hugepage, args=(ip,))
+        p = multiprocessing.Process(target=setup_cpu_memory, args=(ip,))
         p.start()
         pids.append(p)
 
@@ -127,7 +157,7 @@ def setup_hugepage_for_all():
     print("exp: all hugepages ready")
     return
 
-def run_exp():
+def run_cluster_exp():
     # Start all bessd
     pids = []
     for tip in traffic_ip:
@@ -165,11 +195,16 @@ def run_exp():
     return
 
 def main():
+    ## Pre-install
+    # reset_grub_for_all()
     # install_mlnx_for_all()
     # install_bess_for_all()
-    # setup_hugepage_for_all()
 
-    run_exp()
+    ## Config
+    # setup_cpu_hugepage_for_all()
+
+    ## Ready to go
+    run_cluster_exp()
     return
 
 if __name__ == "__main__":
