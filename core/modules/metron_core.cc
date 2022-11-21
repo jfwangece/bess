2fad22abc6c3c521649190f5b75987e8eb7248ff
Jianfeng Wang <pkueewjf@gmail.com>
2022-11-20 22:04:55 -0800

    metron_core: add debug logs

--- core/modules/metron_core.cc
+++ core/modules/metron_core.cc
@@ -14,6 +14,7 @@ CommandResponse MetronCore::Init(const bess::pb::MetronCoreArg& arg) {
   }
 
   local_queue_ = bess::ctrl::local_mc_q[core_id_];
+  LOG(INFO) << "metron: core " << core_id_;
   return CommandSuccess();
 }
 
@@ -26,6 +27,7 @@ void MetronCore::DeInit() {
 struct task_result MetronCore::RunTask(Context *ctx, bess::PacketBatch *batch,
                                      void *) {
   if (local_queue_ == nullptr) {
+    LOG(FATAL) << "metron: local_mc_q is not ready";
     return {.block = false, .packets = 0, .bits = 0};
   }
 
@@ -33,6 +35,7 @@ struct task_result MetronCore::RunTask(Context *ctx, bess::PacketBatch *batch,
   if (cnt == 0) {
     return {.block = false, .packets = 0, .bits = 0};
   }
+  LOG(INFO) << "core: " << core_id_ << ", batch: " << cnt;
 
   batch->set_cnt(cnt);
   uint64_t total_bytes = 0;
