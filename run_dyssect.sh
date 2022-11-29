29592edc5e77ce0721dc6f04376644ddacdf9fc1
Jianfeng Wang <pkueewjf@gmail.com>
2022-11-29 07:28:29 +0000

    dyssect: add a run_script

---
+++ run_dyssect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SHARDS=64                           # The number of shards
+SFC_LENGTH=3                        # The length of the Service Function Chain
+CONTROLLER_CORE=30                  # The core to assign to Dyssect Controller
+SOLVER_CORE=31                      # The core to assign to optimizer process
+SCRIPT_NAME="cloud_dyssect_chain4"  # The name of BESS configuration script (in the bessctl/conf/ directory)
+
+echo "Killing previous processes..."
+pkill -9 bessd 1>/dev/null 2>/dev/null
+pkill -9 solver 1>/dev/null 2>/dev/null
+
+echo "Starting BESS daemon..."
+sudo ./bessctl/bessctl daemon start
+
+echo "Running the optimizer..."
+taskset -c ${SOLVER_CORE} ./solver 1>/dev/null 2>/dev/null &
+
+echo "Running the Dyssect..."
+sudo SHARDS=${SHARDS} SFC_LENGTH=${SFC_LENGTH} CONTROLLER_CORE=${CONTROLLER_CORE} ./bessctl/bessctl run ${SCRIPT_NAME} 1>/dev/null 2>/dev/null
