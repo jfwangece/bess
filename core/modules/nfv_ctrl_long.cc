d184a48a75d0b4241d06159019ed1580730d2a83
Jianfeng Wang <pkueewjf@gmail.com>
2022-12-15 12:56:47 -0800

    nfv_ctrl: start with 2 cores

--- core/modules/nfv_ctrl_long.cc
+++ core/modules/nfv_ctrl_long.cc
@@ -71,7 +71,7 @@ void NFVCtrl::InitPMD(PMDPort* port) {
   port_ = port;
   // start with 0.5x CPU core load
   // active_core_count_ = bess::ctrl::ncore / 2;
-  active_core_count_ = 4;
+  active_core_count_ = 2;
   if (active_core_count_ == 0) {
     active_core_count_ = 1;
   }
